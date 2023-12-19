c23762297acffee0c816674f2bddc6835a24e3fe
Morgan <103436411+MorganKryze@users.noreply.github.com>
2023-12-19 10:45:15 +0100

    🚧 (generator) clean up constants

--- src/generator.py
+++ src/generator.py
@@ -4,26 +4,44 @@ import yaml
 from jinja2 import Environment, FileSystemLoader
 
 
-def read_options(yml_path: str = "src/options.yml") -> dict:
+def read_options(yml_path: str) -> dict:
     """Read the options file and returns a dictionary with the options."""
     with Path.open(yml_path) as file:
         return yaml.safe_load(file)
 
 
-def generate_html(options: dict) -> None:
+def generate_html(
+    options: dict,
+    path_with_photo: str,
+    path_without_photo: str,
+    path_output_file: str,
+) -> None:
     """Generate the HTML file from the options."""
     env = Environment(
         loader=FileSystemLoader("src"),
         autoescape=True,
     )
-    template = env.get_template("signature-logo.html")
+    if "photo" in options:
+        template = env.get_template(path_with_photo)
+    else:
+        template = env.get_template(path_without_photo)
 
     output = template.render(options)
 
-    with Path.open("output.html", "w") as file:
+    with Path.open(path_output_file, "w") as file:
         file.write(output)
 
 
 if __name__ == "__main__":
-    options = read_options()
-    generate_html(options)
+    # Paths
+    options_source = "src/options.yml"
+    html_template_logo = "signature-logo.html"
+    html_template_photo = "signature-photo.html"
+    output_file = "output.html"
+
+    # Generate the HTML file
+    options = read_options(options_source)
+    generate_html(options, html_template_photo, html_template_logo, output_file)
+
+    # Print acknowledgment
+    print("HTML file generated successfully!")
